19de8c30d467f8f6f9c329ad6a6f74bf23c0f337
Jackkii <32056953+jackkii@users.noreply.github.com>
2019-03-28 19:06:52 +0800

    Create frog.py

---
+++ frog.py
@@ -0,0 +1,17 @@
+#frog.py
+#---------------------
+#小青蛙跳台阶
+#----------------------
+#Jackkii   2019/03/28
+#
+
+def jump(n):
+    if n==1:
+        return 1
+    elif n==2:
+        return 2
+    else:
+        return jump(n-1) + jump(n - 2)
+
+n=eval(input())
+print(jump(n))

57d7767a160052f2a15b6e14453cf91e9531f3ce
Jackkii <32056953+jackkii@users.noreply.github.com>
2019-03-29 19:14:40 +0800

    Update SingleLinkedList.py

--- SingleLinkedList.py
+++ SingleLinkedList.py
@@ -1,11 +1,11 @@
-#SingleLinkedList.py
-#---------------------------
-#单向链表
-#--------------------------
-#Jackkii    2019/03/24
+# SingleLinkedList.py
+# ---------------------------
+# 单向链表
+# --------------------------
+# Jackkii    2019/03/24
 #
 
-#定义链表结点
+# 定义链表结点
 class ListNode(object):
     def __init__(self, value, next = None):
         self.val = value
@@ -15,36 +15,36 @@ class ListNode(object):
     def getValue(self):
         return self.val
 
-#定义链表操作
+# 定义链表操作
 class SingleLinkedList:
     def __init__(self):
         self._head = None
         self._size = 0
 
-    #是否为空
+    # 是否为空
     def isempty(self):
         return self._head == None
 
-    #当前结点插入操作
+    # 当前结点插入操作
     def add(self, val):
         node = ListNode(val)
         node.next = self._head
         self._head = node
         self._size += 1
 
-    #表尾插入操作
+    # 表尾插入操作
     def append(self,val):
         node = ListNode(val)
-        if self.isempty():                  #若空链表，则在头部创建结点
+        if self.isempty():                  # 若空链表，则在头部创建结点
             self._head = node
         else:
-            temp = self._head               #临时指针用于寻找尾部结点
+            temp = self._head               # 临时指针用于寻找尾部结点
             while temp.getNext() is not None:
-                temp = temp.getNext()       #遍历链表
-            temp.next = node                  #指向尾部结点
+                temp = temp.getNext()       # 遍历链表
+            temp.next = node                  # 指向尾部结点
         self._size += 1
 
-    #检索元素是否在链表中
+    # 检索元素是否在链表中
     def search(self,val):
         temp = self._head
         iffound = False
@@ -55,7 +55,7 @@ class SingleLinkedList:
                 temp = temp.getNext()
         return iffound
 
-    #寻找元素对应下标index
+    # 寻找元素对应下标index
     def index(self,val):
         temp = self._head
         count = 0
@@ -71,7 +71,7 @@ class SingleLinkedList:
         else:
             raise Exception('%s is not in linkedlist'%val)
 
-    #删除链表中某项元素
+    # 删除链表中某项元素
     def remove(self,val):
         temp = self._head
         pre = None
@@ -87,7 +87,7 @@ class SingleLinkedList:
                 temp =temp.getNext()
         self._size -= 1
 
-    #链表中间插入元素
+    # 链表中间插入元素
     def insert(self,pos,val):
         if pos <= 1:
             self.add(val)
@@ -106,7 +106,7 @@ class SingleLinkedList:
             newnode.next = temp
             self._size += 1
 
-    #打印链表元素
+    # 打印链表元素
     def printlist(self):
         temp = self._head
         while temp is not None:
@@ -115,7 +115,7 @@ class SingleLinkedList:
 
 
 
-#try
+# try
 mylist = SingleLinkedList()
 mylist.insert(1, 3)
 mylist.printlist()
